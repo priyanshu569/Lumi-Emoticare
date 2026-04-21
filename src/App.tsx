90e9bb0d1652ddb204c53595803f0d0f1d2ac629
priyanshu569 <151258245+priyanshu569@users.noreply.github.com>
2026-04-22 02:56:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import AppShell from "./components/AppShell";
+import BottomNav from "./components/BottomNav";
 
 export default function App() {
-  return <AppShell />;
+  return (
+    <div>
+      <h1>Lumi Emoticare </h1>
+      <BottomNav />
+    </div>
+  );
 }
