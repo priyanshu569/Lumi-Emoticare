5d69aac30bf6e0be3cec27a29380f1b184dfd985
priyanshu569 <151258245+priyanshu569@users.noreply.github.com>
2026-04-22 03:22:29 +0530

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,11 @@
+import { AppShell } from "../components/AppShell";
+
 export default function Home() {
   return (
-    <div style={{ padding: "20px", paddingBottom: "100px" }}>
-      <h1>How are you feeling today? 😊</h1>
-      <div style={{ marginTop: "20px" }}>
-        Your current mood: Neutral 😐
+    <AppShell>
+      <div style={{ padding: "20px" }}>
+        <h1>Lumi Emoticare </h1>
       </div>
-    </div>
+    </AppShell>
   );
 }
